0065c068a4274f9a79c463311e3be8c2b96bb10a
vlad <v.khristaforov@atomgroup.io>
2024-02-14 00:19:18 +0300

    fix color

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,5 +1,5 @@
 import {ChangeEventHandler, Dispatch, FC, SetStateAction} from 'react'
-import { TextField } from '@mui/material'
+import {TextField} from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 
 type SearchProps = {
@@ -17,7 +17,11 @@ const Search: FC<SearchProps> = ({value, setValue}) => {
   const color = theme.palette.customColors.bgColor
 
   return <>
-    <TextField value={value} onChange={onChange} sx={{ mt: 2, borderColor: color, color: color }} fullWidth id="outlined-basic"
+    <TextField value={value}   InputProps={{
+      style: {
+        color: 'red'
+      }
+    }} onChange={onChange} sx={{ mt: 2, borderColor: color, color: color }} fullWidth id="outlined-basic"
                label="Поиск" variant="outlined" />
   </>
 }
