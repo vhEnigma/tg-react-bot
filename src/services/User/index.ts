114f85abf7753425e8c84f6697e50156df9915ff
vlad <v.khristaforov@atomgroup.io>
2024-02-14 00:47:41 +0300

    fix

--- src/services/User/index.ts
+++ src/services/User/index.ts
@@ -1,14 +1,14 @@
 import { Endpoints } from '../client/endpoints.ts'
 import { axiosInstance } from '../client/httpClient.ts'
 
-type LoginuserResponseType = {
+type LoginUserResponseType = {
   token: string
 }
 
 
 export class UserService {
   static async loginUserRequest(initDataString: string) {
-    const { data: response } = await axiosInstance.get<LoginuserResponseType>(Endpoints.user.login, { params: { initData: initDataString } })
+    const { data: response } = await axiosInstance.get<LoginUserResponseType>(Endpoints.user.login, { params: { initData: initDataString } })
     return response
   }
 
