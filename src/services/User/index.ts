7bcc3a8f94bab96e78bf589b90d710ed08d7c0db
vlad <v.khristaforov@atomgroup.io>
2024-02-13 12:28:59 +0300

    fix: download new data

--- src/services/User/index.ts
+++ src/services/User/index.ts
@@ -5,7 +5,7 @@ import { axiosInstance } from '../client/httpClient.ts'
 export class UserService {
   static async loginUserRequest(initDataString: string) {
 
-    await axiosInstance.get(Endpoints.user.login, { params: initDataString })
+    await axiosInstance.get(Endpoints.user.login, { params: { initData: initDataString } })
 
     // return response
   }
