ea9cbf4f8b5f92c2651f2632f600406e0ddf6eda
vlad <v.khristaforov@atomgroup.io>
2024-02-13 11:08:31 +0300

    fix: alert

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -9,7 +9,7 @@ const tg = window.Telegram.WebApp
 export const useTelegram = () => {
   tg.expand()
 
-  tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`)
+  tg.showAlert(`Добро пожаловать, @${tg?.username}.`)
   return {
     tg,
     tgUser: tg.initData?.user
