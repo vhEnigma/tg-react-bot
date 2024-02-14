29ed6ae7c2cc994f257f89479bcd4dcb9acc0c7f
vlad <v.khristaforov@atomgroup.io>
2024-02-14 10:57:29 +0300

    fix

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -14,7 +14,7 @@ const Search: FC<SearchProps> = ({value, setValue}) => {
     setValue(event.target.value)
   }
 
-  const color = theme.palette.customColors.bgColor
+  const color = theme.palette.customColors.bgColor.main
 
   return <>
     <TextField value={value} onChange={onChange} sx={{ mt: 2, borderColor: color, color: color }} fullWidth id="outlined-basic"
