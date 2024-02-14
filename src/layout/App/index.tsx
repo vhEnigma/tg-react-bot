7e8c6bf7966f2176d89b97c25a444d9da62e66a7
vlad <v.khristaforov@atomgroup.io>
2024-02-14 14:31:57 +0300

    fix

--- src/layout/App/index.tsx
+++ src/layout/App/index.tsx
@@ -1,5 +1,6 @@
-import { FC, PropsWithChildren, useEffect } from 'react'
+import { FC, PropsWithChildren, Suspense, useEffect } from 'react'
 import { Container } from '@mui/material'
+import { Outlet } from 'react-router-dom'
 import { useTelegram } from '../../hooks/useTelegram.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Loader from '../../components/Loader'
@@ -32,7 +33,9 @@ const App: FC<PropsWithChildren> = () => {
       <ThemeProvider theme={theme}>
         <div className={style.container} style={{backgroundColor: theme.palette.customColors.secondary_bg_color.main}}>
           <Container sx={{ paddingBottom: '56px' }}>
-            <Loader />
+            <Suspense fallback={<Loader />}>
+              <Outlet />
+            </Suspense>
           </Container>
           <Navigation />
         </div>
--- src/pages/Technology/page.tsx
+++ src/pages/Technology/page.tsx
@@ -1,14 +1,10 @@
 import { FC } from 'react'
-import { useTechnology } from '../../hooks/useTechnology.ts'
 import styles from './styles.module.css'
 import Loader from '../../components/Loader'
 
-type TechnologyProps = {}
 
-const Technology: FC<TechnologyProps> = () => {
-  const { isLoading } = useTechnology()
-
-  if (isLoading) return <Loader />
+const Technology: FC = () => {
+  return <Loader />
   return <div className={styles.container}>
     technology
   </div>
