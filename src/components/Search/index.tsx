fc04ad0a3ffb7422130422651e33b7674b53379a
vlad <v.khristaforov@atomgroup.io>
2024-02-14 11:18:58 +0300

    fix

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -13,11 +13,11 @@ const Search: FC<SearchProps> = ({value, setValue}) => {
   const onChange:ChangeEventHandler<HTMLInputElement> = (event) => {
     setValue(event.target.value)
   }
-
-  const color = theme.palette.customColors.bg_color.main
+  const {bg_color, text_color, section_bg_color,} = theme.palette.customColors
+  const color = bg_color.main
 
   return <>
-    <TextField value={value} onChange={onChange} InputLabelProps={{style:{color: theme.palette.customColors.text_color.main}}} InputProps={{style:{color: theme.palette.customColors.text_color.main, backgroundColor: theme.palette.customColors.section_bg_color.main}}} sx={{ mt: 2, borderColor: color, backgroundColor: color }} fullWidth id="outlined-basic"
+    <TextField value={value} onChange={onChange} InputLabelProps={{style:{color: text_color.main}}} InputProps={{style:{color: text_color.main, backgroundColor: section_bg_color.main}}} sx={{ mt: 2, borderColor: color, backgroundColor: color }} fullWidth id="outlined-basic"
                label="Поиск" variant="outlined" />
   </>
 }
