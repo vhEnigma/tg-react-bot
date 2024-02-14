a4b306a00d529106aeda06898b3188d7f9a300c3
vlad <v.khristaforov@atomgroup.io>
2024-02-14 10:36:07 +0300

    fix

--- src/services/Direction/index.ts
+++ src/services/Direction/index.ts
@@ -1,16 +1,15 @@
-// import { Endpoints } from '../client/endpoints.ts'
 import { axiosInstance } from '../client/httpClient.ts'
+import {Endpoints} from "../client/endpoints.ts";
 
-// type DirectionsType = {
-//   id: number
-//   name: string
-// }
+type DirectionsType = {
+  id: number
+  name: string
+}
 
-// type ListDirectionsResponseType = {
-//   page: number
-//   result: DirectionsType[]
-// }
-let count = 0
+type ListDirectionsResponseType = {
+  page: number
+  result: DirectionsType[]
+}
 
 type ParamsListDirectionsRequest = {
   searchValue?: string
@@ -19,36 +18,35 @@ type ParamsListDirectionsRequest = {
 
 export class DirectionService {
   static async listDirectionRequest(params:ParamsListDirectionsRequest) {
-    const {searchValue, page} = params
+    const {page} = params
+    //
+    // const url = new URL('https://jsonplaceholder.typicode.com/users');
+    // const searchParams = new URLSearchParams();
+    // console.log(url)
+    // if (searchValue) {
+    //   searchParams.append('search', searchValue);
+    // }
+    //
+    // if (page) {
+    //   searchParams.append('page', page.toString());
+    // }
+    //
+    // url.search = searchParams.toString();
+    //
+    // console.log(url, 'URL')
+    // // const pageNumber = page || 1
+    // count++
+    //
+    // if (count === 4) {
+    //   return {result: []}
+    // }
 
-    const url = new URL('https://jsonplaceholder.typicode.com/users');
-    const searchParams = new URLSearchParams();
-
-    if (searchValue) {
-      searchParams.append('search', searchValue);
-    }
-
-    if (page) {
-      searchParams.append('page', page.toString());
-    }
-
-    url.search = searchParams.toString();
-
-    console.log(url, 'URL')
-    // const pageNumber = page || 1
-    count++
-
-    if (count === 4) {
-      return {result: []}
-    }
-
-    // const url = `${Endpoints.directions}?page=${pageNumber}&pageSize=100`
+    const url = `${Endpoints.directions}?page=${page}&pageSize=100`
     // const url = `https://jsonplaceholder.typicode.com/users`
 
-    // const { data: response } = await axiosInstance.get<ListDirectionsResponseType>(url)
-    const { data: response } = await axiosInstance.get(url.toString())
+    const { data: response } = await axiosInstance.get<ListDirectionsResponseType>(url)
+    // const { data: response } = await axiosInstance.get(url.toString())
     return {result: response}
-    // return response
   }
 
 }
