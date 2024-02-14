1b20de555f11deb1e8056f109445f2f059ce8e7a
vlad <v.khristaforov@atomgroup.io>
2024-02-14 17:30:50 +0300

    fix

--- src/pages/Root/page.tsx
+++ src/pages/Root/page.tsx
@@ -4,10 +4,10 @@ import {Box, Typography} from "@mui/material";
 import useTgTheme from "../../hooks/useTgTheme.ts";
 
 const Root: FC = () => {
-  const {user} = useTelegram()
+  const {user:{username, first_name}} = useTelegram()
   const {text_color} = useTgTheme()
 
-  const title = `${user.first_name} (${user.username})`
+  const title = `${first_name} (${username})`
   return <Box>
     <Typography component='h1' sx={{margin: '20px 0', textAlign: 'center', color: text_color}}>
     {title}
