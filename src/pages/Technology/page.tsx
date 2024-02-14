0bf6b44a7cfd3ef8d153a55ee11867d913d13506
vlad <v.khristaforov@atomgroup.io>
2024-02-14 15:58:29 +0300

    fix

--- src/pages/Technology/page.tsx
+++ src/pages/Technology/page.tsx
@@ -1,13 +1,9 @@
 import { FC } from 'react'
-import styles from './styles.module.css'
 import Loader from '../../components/Loader'
 
 
 const Technology: FC = () => {
   return <Loader />
-  return <div className={styles.container}>
-    technology
-  </div>
 }
 
 export default Technology
