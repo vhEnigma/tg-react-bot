9d0a61112624fe68031f2597b3d6f8c739a1a067
vlad <v.khristaforov@atomgroup.io>
2024-02-14 16:54:35 +0300

    fix

--- src/pages/Root/page.tsx
+++ src/pages/Root/page.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react'
 import {useTelegram} from "../../hooks/useTelegram.ts";
+import {Box, Typography} from "@mui/material";
 
-type RootProps = {}
-
-const Root: FC<RootProps> = () => {
+const Root: FC = () => {
   const {tg} = useTelegram()
-  return <>
+  return <Box>
+    <Typography component='h1' sx={{m: '0 auto'}}>
     {tg.initDataUnsafe.user.first_name}
-  </>
+  </Typography>
+  </Box>
 }
 
 export default Root
