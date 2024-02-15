7726c861d6bc12f6d3276b2f904eacc59872c82c
vlad <v.khristaforov@atomgroup.io>
2024-02-15 11:35:07 +0300

    fix

--- src/pages/ErrorBoundary/index.tsx
+++ src/pages/ErrorBoundary/index.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { Button, Container, Typography } from '@mui/material'
 import {useLocation, useNavigate} from 'react-router-dom'
 import { RouteList } from '../../routes/routes.ts'
-import { useTheme } from '@mui/material/styles'
 
 
 const styles = {
@@ -14,7 +13,6 @@ const styles = {
 }
 
 const ErrorBoundary: FC = () => {
-  const theme = useTheme()
   const navigate = useNavigate()
   const location = useLocation()
   console.log(location)
@@ -26,7 +24,7 @@ const ErrorBoundary: FC = () => {
     <Typography>
       Что-то пошло не так...
     </Typography>
-    <Button onClick={returnHandle} sx={{ backgroundColor: theme.palette.customColors.bg_color }} variant="contained">На
+    <Button onClick={returnHandle} variant="contained">На
       главную</Button>
   </Container>
 }
