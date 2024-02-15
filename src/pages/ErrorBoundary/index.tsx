b826f1d865fb5138c5dcf848d8d340ef5c8f0096
vlad <v.khristaforov@atomgroup.io>
2024-02-15 12:01:00 +0300

    fix

--- src/pages/ErrorBoundary/index.tsx
+++ src/pages/ErrorBoundary/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Button, Container, Typography } from '@mui/material'
-import {useLocation, useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import { RouteList } from '../../routes/routes.ts'
 
 
@@ -14,8 +14,7 @@ const styles = {
 
 const ErrorBoundary: FC = () => {
   const navigate = useNavigate()
-  const location = useLocation()
-  console.log(location)
+
   const returnHandle = () => {
     navigate(RouteList.Root)
   }
