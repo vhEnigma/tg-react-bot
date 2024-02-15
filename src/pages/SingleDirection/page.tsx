9439889f58c57a33586514379ed2781ac8685c9f
vlad <v.khristaforov@atomgroup.io>
2024-02-15 15:07:32 +0300

    fix

--- src/pages/SingleDirection/page.tsx
+++ src/pages/SingleDirection/page.tsx
@@ -7,14 +7,27 @@ import Loader from "../../components/Loader";
 import Search from "../../components/Search";
 import useTgTheme from "../../hooks/useTgTheme.ts";
 
+const tabsConfig = [
+  {
+    id: 1,
+    title: 'Статьи',
+    key: 'article'
+  },
+  {
+    id: 2,
+    title: 'Тесты',
+    key: 'tests'
+  }
+]
 
 const SingleDirection: FC = () => {
   const {id} = useParams()
-  const {button_color, button_text_color, text_color} = useTgTheme()
+  const {button_color, button_text_color, text_color, bg_color, link_color} = useTgTheme()
   const [isLoading, setLoading] = useState(false)
   const [articleList, setArticleList] = useState<ArticleType[]>([])
   const [title, setTitle] = useState('')
   const [searchValue, setSearchValue] = useState('')
+  const [activeTab, setActiveTab] = useState('article')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,11 +54,20 @@ const SingleDirection: FC = () => {
   //   })
   // }
 
+  const renderTabs = () => {
+    return tabsConfig.map((options) => {
+      const {id, title, key} = options
+      const isActive = key === activeTab
+      const backgroundColor = isActive ? bg_color : button_color
+      const color = isActive ? link_color : button_text_color
+      return <Button key={id} onClick={() => setActiveTab(key)} fullWidth sx={{backgroundColor, color}} variant="contained">{title}</Button>
+    })
+  }
+
   return <Box>
     <Typography component='h1' sx={{color: text_color, textAlign: 'center', mt: '20px', textTransform: 'uppercase'}}>{title}</Typography>
-    <Container sx={{display: 'flex', justifyContent: 'space-between'}}>
-      <Button sx={{background: button_color, color: button_text_color}} variant="contained">Статьи</Button>
-      <Button sx={{background: button_color, color: button_text_color}} variant="contained">Тесты</Button>
+    <Container sx={{display: 'flex', justifyContent: 'space-between', gap: '50px'}}>
+      {renderTabs()}
     </Container>
     <Search value={searchValue} setValue={setSearchValue} />
   </Box>
