9645a6446cf1b5406260d2e8eea04ac1a46202fa
vlad <v.khristaforov@atomgroup.io>
2024-02-18 13:52:39 +0300

    fix

--- src/components/MenuList/index.tsx
+++ src/components/MenuList/index.tsx
@@ -27,7 +27,7 @@ const MenuList: FC<DirectionsProps> = ({ route, queryKey, request }) => {
   const { searchList, setSearchList, setSearchValue, debouncedSearchValue, isSearch, setSearch, searchValue } = useSearch<MenuListType[]>()
   const navigate = useNavigate()
   const {
-    isFetching: isLoading,
+    isPending: isLoading,
     data: response,
     isSuccess
   } = useMenuList({
