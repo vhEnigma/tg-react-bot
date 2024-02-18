9092ab30327dbe857497e55f1c33ed16fbd61610
vlad <v.khristaforov@atomgroup.io>
2024-02-18 14:42:01 +0300

    fix

--- src/components/MenuList/index.tsx
+++ src/components/MenuList/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from 'react'
 import { Box, List, ListItemButton, ListItemText } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { QueryCache } from '@tanstack/react-query'
 import Search from '../Search'
 import Loader from '../Loader'
 import { MenuListType } from '../../types/menuList'
@@ -18,6 +19,7 @@ type DirectionsProps = {
   route: string
   queryKey: string
 }
+const queryCache = new QueryCache()
 
 const MenuList: FC<DirectionsProps> = ({ route, queryKey, request }) => {
   const { ref, inView, setStopInfinityScroll, isStopInfinityScroll, downloadedPages, setDownloadedPages } = useInfinityScroll()
@@ -31,6 +33,9 @@ const MenuList: FC<DirectionsProps> = ({ route, queryKey, request }) => {
     params: { page: downloadedPages },
     queryKey
   })
+
+  const query = queryCache.findAll({ queryKey: [queryKey] })
+  console.log(query, 'FUCKING CAHCE')
 
   useEffect(() => {
     if (!isSuccess) return
