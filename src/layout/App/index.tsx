cec3c160272ac3d60b193b35a70f916efcde840a
vlad <v.khristaforov@atomgroup.io>
2024-02-18 11:06:28 +0300

    fix

--- src/layout/App/index.tsx
+++ src/layout/App/index.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren, Suspense, useEffect } from 'react'
 import {Box, Container} from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import { useTelegram } from '../../hooks/useTelegram.ts'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Loader from '../../components/Loader'
 import style from './style.module.css'
 // import { UserService } from '../../services/User'
@@ -14,7 +14,6 @@ import {NAVIGATION_HEIGHT} from "../../constants/style.ts";
 import useLoginUser from "../../hooks/tanstack/useLoginUser.ts";
 
 
-const queryClient = new QueryClient()
 const App: FC<PropsWithChildren> = () => {
   const { tg } = useTelegram()
   useLoginUser(tg.initData)
@@ -32,7 +31,6 @@ const App: FC<PropsWithChildren> = () => {
   const theme = createCustomTheme(tg)
 
   return (
-    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <Box className={style.container} style={{backgroundColor: theme.palette.customColors.secondary_bg_color.main}}>
           <Container sx={{height: `calc(100vh - ${NAVIGATION_HEIGHT}px)`, overflow: 'auto'}}>
@@ -43,7 +41,6 @@ const App: FC<PropsWithChildren> = () => {
           <Navigation />
         </Box>
       </ThemeProvider>
-    </QueryClientProvider>
   )
 }
 
