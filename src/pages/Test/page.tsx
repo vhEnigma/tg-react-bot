694740fb9922c30575036da8fc31eb69626f1369
vlad <v.khristaforov@atomgroup.io>
2024-02-20 18:33:40 +0300

    fix

--- src/pages/Test/page.tsx
+++ src/pages/Test/page.tsx
@@ -64,7 +64,7 @@ const Test: FC = () => {
             {index + 1}. {text}
           </Typography>
           <Box sx={{ width: '100%' }}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            <Grid container rowSpacing={1} columnSpacing={1}>
               {answers}
             </Grid>
           </Box>
