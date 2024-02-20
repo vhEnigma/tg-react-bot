f0c9094adcef14805db98941c762436d9220752a
vlad <v.khristaforov@atomgroup.io>
2024-02-20 18:15:14 +0300

    fix

--- src/pages/Test/page.tsx
+++ src/pages/Test/page.tsx
@@ -38,7 +38,7 @@ const Test: FC = () => {
       return (
         <Grid key={id} item xs={6}>
           <Box sx={{ display: 'flex', gap: '10px' }}>
-            <Checkbox sx={{ backgroundColor: section_bg_color }} inputProps={{ 'aria-label': 'Checkbox demo' }} />
+            <Checkbox sx={{ backgroundColor: section_bg_color }} />
             <Typography sx={{ color: text_color }}>{text}</Typography>
           </Box>
         </Grid>
