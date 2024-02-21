1c99341cfd1cddabe63dce3d8e5834386ac53b46
vlad <v.khristaforov@atomgroup.io>
2024-02-21 17:12:20 +0300

    fix

--- src/services/ArticleService/index.ts
+++ src/services/ArticleService/index.ts
@@ -1,13 +1,10 @@
 import { Endpoints } from '../client/endpoints'
 import { axiosInstance } from '../client/httpClient'
-import { IParams } from '../../types/params'
-import { getQueryString } from '../../utils/params'
 import { ArticleType } from '../../types/menuList'
 
 export class ArticleService {
-  static async getSingleArticle(params: IParams) {
-    const queryString = getQueryString(params)
-    const url = `${Endpoints.article.getSingle}${queryString}`
+  static async getSingleArticle(id: string) {
+    const url = `${Endpoints.article.getSingle}/${id}`
 
     const { data: response } = await axiosInstance.get<ArticleType>(url)
 
