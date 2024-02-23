a3c2c544a0deb5bff5a468b5596edd578f5a7c85
vlad <v.khristaforov@atomgroup.io>
2024-02-23 14:56:28 +0300

    fix

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -4,7 +4,8 @@ export const getActiveRoute = (pathname: string) => {
   const valuesMap: Record<string, number> = {
     [RouteList.Root]: 0,
     [RouteList.Directions]: 1,
-    [RouteList.Technology]: 2
+    [RouteList.Technology]: 2,
+    [RouteList.New]: 3
   }
 
   const key = pathname.slice(1) || RouteList.Root
