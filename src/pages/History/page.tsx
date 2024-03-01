92c4cef9d98b4cbf965dd2f19858038823f8f78a
vlad <v.khristaforov@atomgroup.io>
2024-03-01 11:14:00 +0300

    fix

--- src/pages/History/page.tsx
+++ src/pages/History/page.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Box, ListItemButton, ListItemText } from '@mui/material'
+import { Box, List, ListItemButton, ListItemText } from '@mui/material'
 import useBackButton from '../../hooks/useBackButton'
 import { RouteList } from '../../routes/routes'
 import Catalog from '../../components/Catalog'
@@ -8,13 +8,11 @@ import { ArticleService } from '../../services/ArticleService'
 import { TestService } from '../../services/TestService'
 import { RenderItemsProps } from '../../components/InfinityScrollList'
 import { ArticleType, TestType } from '../../types/menuList'
-import { MultiLineEllipsisStyle } from '../../constants/style'
-import MenuItemInfo from '../../components/MenuItemInfo'
 import ArticleCard from '../../components/ArticleCard'
 import useTgTheme from '../../hooks/useTgTheme'
 
 const History: FC = () => {
-  const { bg_color, button_color } = useTgTheme()
+  const { button_color } = useTgTheme()
   const navigate = useNavigate()
   useBackButton()
 
@@ -25,37 +23,26 @@ const History: FC = () => {
   const renderTests = (props: RenderItemsProps<TestType>) => {
     const { ref, dataList } = props
     const lastIndex = dataList.length - 1
-    return dataList.map((test, index) => {
-      const { id, name, rating } = test
-      const content = (
-        <>
-          <ListItemText sx={MultiLineEllipsisStyle} primary={name} />
-          <MenuItemInfo rating={rating} />
-        </>
-      )
 
-      if (lastIndex === index) {
-        return (
-          <ListItemButton
-            key={id}
-            ref={ref}
-            onClick={() => navigate(`/${RouteList.Test}/${id}`)}
-            sx={{ borderTop: `1px solid ${button_color}`, backgroundColor: bg_color }}
-          >
-            {content}
-          </ListItemButton>
-        )
-      }
-      return (
-        <ListItemButton
-          key={id}
-          onClick={() => navigate(`/${RouteList.Test}/${id}`)}
-          sx={{ borderTop: `1px solid ${button_color}`, backgroundColor: bg_color }}
-        >
-          {content}
-        </ListItemButton>
-      )
-    })
+    return (
+      <List component='div'>
+        {dataList.map(({ id, name }, index) => {
+          const displayValue = `${name} - ${0}% (X) раз пройден`
+          if (lastIndex === index) {
+            return (
+              <ListItemButton ref={ref} key={id} onClick={() => openTestHande(id)} sx={{ borderTop: `1px solid ${button_color}` }}>
+                <ListItemText primary={displayValue} />
+              </ListItemButton>
+            )
+          }
+          return (
+            <ListItemButton key={id} onClick={() => openTestHande(id)} sx={{ borderTop: `1px solid ${button_color}` }}>
+              <ListItemText primary={displayValue} />
+            </ListItemButton>
+          )
+        })}
+      </List>
+    )
   }
 
   const renderArticles = (props: RenderItemsProps<ArticleType>) => {
@@ -70,8 +57,6 @@ const History: FC = () => {
       return <ArticleCard onCLick={() => navigate(`/${RouteList.Article}/${article.id}`)} key={article.id} article={article} />
     })
   }
-
-  console.log(openTestHande)
 
   return (
     <Box sx={{ pt: '10px' }}>
