0ee794d2cee32e79b0ff273c3750ac5693e98dfc
vlad <v.khristaforov@atomgroup.io>
2024-03-01 19:04:22 +0300

    fix

--- src/components/InfinityScrollList/index.tsx
+++ src/components/InfinityScrollList/index.tsx
@@ -35,7 +35,7 @@ const InfinityScrollList = <T extends MenuItemType>({ requestId, activeTab, rend
   }
 
   useEffect(() => {
-    if (activeTab) {
+    if (activeTab && downloadedPages !== 1) {
       setDownloadedPages(1)
       setDataList([])
       setStopInfinityScroll(false)
