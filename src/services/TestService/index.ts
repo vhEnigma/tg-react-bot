475a0ef3c4064ef4d36bff573a4b03fbc2383c5a
vlad <v.khristaforov@atomgroup.io>
2024-03-05 18:47:06 +0300

    fix

--- src/services/TestService/index.ts
+++ src/services/TestService/index.ts
@@ -15,7 +15,7 @@ export class TestService {
   }
 
   static async getTestResultById(id: string) {
-    const url = `${Endpoints.tests}${id}/test_results`
+    const url = `${Endpoints.tests}/${id}/test_results`
 
     const { data: response } = await axiosInstance.get<ResultResponseType<TestType>>(url)
 
