6a08c568737f362f509a5e6e37a93d9b46fb0222
vlad <v.khristaforov@atomgroup.io>
2024-03-06 13:09:23 +0300

    fix

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -9,7 +9,7 @@ export const getIntervalDate = (days: number) => {
   const today = new Date()
 
   return {
-    from: pastDate.getTime() * 1000,
-    to: today.getTime() * 1000
+    from: Math.floor(pastDate.getTime() / 1000),
+    to: Math.floor(today.getTime() / 1000)
   }
 }
