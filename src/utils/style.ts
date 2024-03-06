556f7c414f1be698b9a386a72607630648876e3b
vlad <v.khristaforov@atomgroup.io>
2024-03-06 12:26:48 +0300

    fix

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -1,3 +1,3 @@
 import { NAVIGATION_HEIGHT } from '../constants/style'
 
-export const calcLoaderWrapperHeight = (height: number) => `calc(100vh - ${NAVIGATION_HEIGHT}px - ${height}px)`
+export const calcLoaderWrapperHeight = (height: number) => `calc(100% - ${NAVIGATION_HEIGHT}px - ${height}px)`
