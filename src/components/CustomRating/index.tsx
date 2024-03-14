f1a7735f2f807c3acff045c9883569b05c0be174
vlad <v.khristaforov@atomgroup.io>
2024-03-14 17:52:00 +0300

    fix

--- src/components/CustomRating/index.tsx
+++ src/components/CustomRating/index.tsx
@@ -1,37 +1,37 @@
-import React, { FC, SyntheticEvent } from 'react'
-import { StarOutline } from '@mui/icons-material'
-import { Box, Rating } from '@mui/material'
-import { styled } from '@mui/material/styles'
+import React, {FC, SyntheticEvent} from 'react'
+import {StarOutline} from '@mui/icons-material'
+import {Box, Rating} from '@mui/material'
+import {styled} from '@mui/material/styles'
 import useTgTheme from '../../hooks/useTgTheme'
 
 type RatingProps = {
-  rating: number
-  onChange: (event: SyntheticEvent<Element, Event>, newValue: number | null) => Promise<void>
+    rating: number
+    onChange: (event: SyntheticEvent<Element, Event>, newValue: number | null) => Promise<void>
 }
 
-const CustomRating: FC<RatingProps> = ({ rating, onChange }) => {
-  const { link_color, button_color, accent_text_color } = useTgTheme()
+const CustomRating: FC<RatingProps> = ({rating, onChange}) => {
+    const {link_color, button_color, accent_text_color} = useTgTheme()
 
-  const StyledRating = styled(Rating)({
-    '& .MuiRating-iconFilled': {
-      color: button_color
-    },
-    '& .MuiRating-iconHover': {
-      color: accent_text_color
-    }
-  })
+    const StyledRating = styled(Rating)({
+        '& .MuiRating-iconFilled': {
+            color: button_color
+        },
+        '& .MuiRating-iconHover': {
+            color: accent_text_color
+        }
+    })
 
-  return (
-    <Box sx={{ textAlign: 'center' }}>
-      <StyledRating
-        name='simple-controlled'
-        value={rating}
-        onChange={onChange}
-        size='large'
-        emptyIcon={<StarOutline sx={{ borderColor: 'red', color: link_color }} fontSize='inherit' />}
-      />
-    </Box>
-  )
+    return (
+        <Box sx={{textAlign: 'center'}}>
+            <StyledRating
+                name='simple-controlled'
+                value={rating}
+                onChange={onChange}
+                size='large'
+                emptyIcon={<StarOutline sx={{color: link_color}} fontSize='inherit'/>}
+            />
+        </Box>
+    )
 }
 
 export default CustomRating
--- src/components/MenuItemInfo/index.tsx
+++ src/components/MenuItemInfo/index.tsx
@@ -1,64 +1,64 @@
-import React, { FC } from 'react'
-import { Box, Typography } from '@mui/material'
+import React, {FC} from 'react'
+import {Box, Typography} from '@mui/material'
 import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded'
 import useTgTheme from '../../hooks/useTgTheme'
 
 type MenuItemInfoProps = {
-  rating: number
-  reading_time?: number
-  info?: string[]
-  withTimeEllipsis?: boolean
+    rating: number
+    reading_time?: number
+    info?: string[]
+    withTimeEllipsis?: boolean
 }
 
-const ellipsisStyle = { overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }
+const ellipsisStyle = {overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}
 
-const MenuItemInfo: FC<MenuItemInfoProps> = ({ reading_time, rating, info, withTimeEllipsis = false }) => {
-  const { text_color, button_color } = useTgTheme()
+const MenuItemInfo: FC<MenuItemInfoProps> = ({reading_time, rating, info, withTimeEllipsis = false}) => {
+    const {text_color, button_color} = useTgTheme()
 
-  const getInfo = (array?: string[]) => {
-    if (Array.isArray(array)) {
-      return array.map((item, index) => (
-        <Typography key={index} sx={{ color: text_color }}>
-          {item}&#160;|&#160;
-        </Typography>
-      ))
+    const getInfo = (array?: string[]) => {
+        if (Array.isArray(array)) {
+            return array.map((item, index) => (
+                <Typography key={index} sx={{color: text_color}}>
+                    {item}&#160;|&#160;
+                </Typography>
+            ))
+        }
+
+        return ''
     }
 
-    return ''
-  }
+    const getReadingTime = () => {
+        if (reading_time) {
+            const maxWidth = withTimeEllipsis ? '100%' : '48px'
+            return (
+                <>
+                    <Typography
+                        sx={{
+                            color: text_color,
+                            ...ellipsisStyle,
+                            minWidth: '48px',
+                            maxWidth
+                        }}
+                    >
+                        {reading_time} мин.
+                    </Typography>
+                    <Typography sx={{color: text_color}}> | </Typography>
+                </>
+            )
+        }
 
-  const getReadingTime = () => {
-    if (reading_time) {
-      const maxWidth = withTimeEllipsis ? '100%' : '48px'
-      return (
-        <>
-          <Typography
-            sx={{
-              color: text_color,
-              ...ellipsisStyle,
-              minWidth: '48px',
-              maxWidth
-            }}
-          >
-            {reading_time} мин.
-          </Typography>
-          <Typography sx={{ color: text_color }}> | </Typography>
-        </>
-      )
+        return ''
     }
-
-    return ''
-  }
-  return (
-    <Box sx={{ display: 'flex', gap: '5px', ml: '10px' }}>
-      {getInfo(info)}
-      {getReadingTime()}
-      <Typography sx={{ color: text_color, ...ellipsisStyle, minWidth: '40px', maxWidth: '40px' }} component='span'>
-        {rating}/5
-      </Typography>
-      <StarBorderRoundedIcon sx={{ color: button_color }} />
-    </Box>
-  )
+    return (
+        <Box sx={{display: 'flex', gap: '5px', ml: '10px'}}>
+            {getInfo(info)}
+            {getReadingTime()}
+            <Typography sx={{color: text_color, ...ellipsisStyle, minWidth: '40px', maxWidth: '40px'}} component='span'>
+                {rating}/5
+            </Typography>
+            <StarBorderRoundedIcon sx={{color: button_color}}/>
+        </Box>
+    )
 }
 
 export default MenuItemInfo
