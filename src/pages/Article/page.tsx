533d9e6f3cf369cb6e7d4b5d8fdddf75d13f3533
vlad <v.khristaforov@atomgroup.io>
2024-03-19 12:30:27 +0300

    fix

--- src/pages/Article/page.tsx
+++ src/pages/Article/page.tsx
@@ -174,7 +174,7 @@ const Article: FC = () => {
                 <AccordionSummary expandIcon={<ExpandMoreIcon/>} aria-controls='panel2-content' id='panel2-header'>
                     <Typography>Подробнее</Typography>
                 </AccordionSummary>
-                <AccordionDetails sx={{color: 'red'}}>
+                <AccordionDetails sx={{color: 'red !important', backgroundColor: 'red'}}>
                     <ArticleCard article={article} withOutCLick/>
                 </AccordionDetails>
             </Accordion>
