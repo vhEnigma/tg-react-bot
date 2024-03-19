62117068570862d4f81c1efc78f1fa7de96878ad
vlad <v.khristaforov@atomgroup.io>
2024-03-19 14:45:32 +0300

    fix

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,44 +1,47 @@
-import { createTheme } from '@mui/material/styles'
+import {createTheme} from '@mui/material/styles'
 
 interface CustomColors {
-  [key: string]: {
-    main: string
-    light?: string
-    dark?: string
-    contrastText?: string
-  }
+    [key: string]: {
+        main: string
+        light?: string
+        dark?: string
+        contrastText?: string
+    }
 }
 
 declare module '@mui/material/styles' {
-  interface Palette {
-    customColors: CustomColors
-  }
+    interface Palette {
+        customColors: CustomColors
+    }
 
-  interface PaletteOptions {
-    customColors?: CustomColors
-  }
+    interface PaletteOptions {
+        customColors?: CustomColors
+    }
 }
 
 interface ThemeParams {
-  [key: string]: string
+    [key: string]: string
 }
 
 export const createCustomTheme = (tg: { themeParams: ThemeParams }) => {
-  const customColors: Record<string, { main: string }> = {}
-  const colorKeys = Object.keys(tg.themeParams)
+    const customColors: Record<string, { main: string }> = {}
+    const colorKeys = Object.keys(tg.themeParams)
 
-  colorKeys.forEach((key) => {
-    customColors[key] = {
-      main: tg.themeParams[key]
-    }
-  })
+    colorKeys.forEach((key) => {
+        customColors[key] = {
+            main: tg.themeParams[key]
+        }
+    })
 
-  return createTheme({
-    palette: {
-      customColors,
-      primary: {
-        main: tg.themeParams.button_color
-      }
-    }
-  })
+    return createTheme({
+        typography: {
+            fontFamily: 'OpenSans'
+        },
+        palette: {
+            customColors,
+            primary: {
+                main: tg.themeParams.button_color
+            }
+        }
+    })
 }
